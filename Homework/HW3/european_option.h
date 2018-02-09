930187eb1ff7e2643b5512e99a863816af327d1a
Park <wanbaep2@ug-mac34.ics.illinois.edu>
2018-02-09 17:51:44 -0600

    making EuropeanOption class

---
+++ Homework/HW3/european_option.h
@@ -0,0 +1,10 @@
+#ifndef _EUROPEAN_OPTION_H_
+#define _EUROPEAN_OPTION_H_
+#include "option.h"
+class EuropeanOption {
+public:
+
+private:
+    double s_, strike_, r_, div_, t_, sigma_;
+};
+#endif
