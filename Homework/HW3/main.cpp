0f551e4dd56cb3a1755681d73326b770018ba79e
Park <wanbaep2@ug-mac40.ics.illinois.edu>
2018-02-13 22:08:00 -0600

    add get extrapolation value

--- Homework/HW3/main.cpp
+++ Homework/HW3/main.cpp
@@ -4,11 +4,12 @@
 #include "plainvanilla_option.h"
 #include "american_option.h"
 #include "marketvariable.h"
+#include "u_math.h"
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
+    if (argc != 5) {
         std::cout << "Input error!" << std::endl;
-        std::cout << "Usage: ./filename   CRRRBoutput.csv  LRoutput.csv" << std::endl;
+        std::cout << "Usage: ./filename   CRRRBoutput.csv  LRoutput.csv  CRRRBextrapolation.csv  LRextrapolation.csv" << std::endl;
         exit(1);
     }
 
@@ -44,11 +45,41 @@ int main(int argc, char **argv) {
             lrout << i << "," << (lrprice - bsp) << std::endl;
         }
     }
-    std::cout << "Calculation completed successfully." << std::endl;
+    std::cout << "Error Calculation completed successfully." << std::endl;
 
     /* Close output stream */
     crrrbout.close();
     lrout.close();
 
+    /* Extrapolation Price */
+
+    std::ofstream crrRbExtra, lrExtra;
+    crrRbExtra.open(argv[3], std::ios::app);
+    lrExtra.open(argv[4],std::ios::app);
+    crrRbExtra << "N, M, CRR, RB" << std::endl;
+    lrExtra << "N, M, LR" << std::endl;
+    int stepSize1 = 25;                             /* Step size for CRR and RB */
+    int stepSize2 = 50;                             /* Step size for LR */
+    for (int i = 1; i <= 25; ++i) {
+        double crrValue1 = option1.bntprice(i * stepSize1, CRR);
+        double crrValue2 = option1.bntprice(2 * i * stepSize1, CRR);
+        double rbValue1 = option1.bntprice(i * stepSize1, RB);
+        double rbValue2 = option1.bntprice(2 * i * stepSize1, RB);
+        double crrExtraValue = extrapolate(i * stepSize1, crrValue1, 2 * i * stepSize1, crrValue2);
+        double rbExtraValue = extrapolate(i * stepSize1, rbValue1, 2 * i * stepSize1, rbValue2);
+        crrRbExtra << i * stepSize1 << "," << 2 * i * stepSize1 << "," << (crrExtraValue - bsp) << "," << (rbExtraValue - bsp) << std::endl;
+    }
+
+    for (int i = 1; i <= 10; ++i) {
+        double lrValue1 = option1.bntprice(i * stepSize2 + 1, LR);
+        double lrValue2 = option1.bntprice(2 * i * stepSize2 + 1, LR);
+        double lrExtraValue = extrapolate(i * stepSize2 + 1, lrValue1, 2 * i * stepSize2 + 1, lrValue2);
+        lrExtra << i * stepSize2 + 1 << "," << 2 * i * stepSize2 + 1 << "," << (lrExtraValue - bsp) << std::endl;
+    }
+
+    std::cout << "Extrpolation completed successfully." << std::endl;
+
+    crrRbExtra.close();
+    lrExtra.close();
     return 0;
 }
