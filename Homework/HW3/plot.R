31f25fffc366aba9351a9a3fc6e45f75e52d00e2
Park <wanbaep2@ug-mac36.ics.illinois.edu>
2018-02-16 17:11:38 -0600

    problem 2 plot

--- Homework/HW3/plot.R
+++ Homework/HW3/plot.R
@@ -1,4 +1,6 @@
-setwd("/Users/wanbaep2/Desktop/FIN514/Homework/HW3")
+#------------------------------------------------------------------------------
+## PROBLEM 1
+setwd("/Users/wanbaep2/Desktop/FIN514/Homework/HW3/Q1")
 error1 = read.csv("out1.csv") #CRR, RB ERROR
 error2 = read.csv("out2.csv") #LR ERROR
 error3 = read.csv("out3.csv") #CRR, RB Extrapolation
@@ -19,3 +21,34 @@ plot(error3$N, error3$RB, type = 'l', main = "Error of Extrapolation: RB",
      xlab = "N", ylab = "ERROR")
 plot(error4$N, error4$LR, type = 'l', main = "Error of Extrapolation: LR",
      xlab = "N", ylab = "ERROR")
+
+#------------------------------------------------------------------------------
+## PROBLEM 2
+setwd("/Users/wanbaep2/Desktop/FIN514/Homework/HW3/Q2")
+error5 = read.csv("out1.csv") #CRR, BD ERROR
+error6 = read.csv("out2.csv") #LR ERROR
+bound = read.csv("out3.csv") #Exercise bound of option
+error7 = read.csv("out4.csv") #Error of Extrapolation: CRR, BD
+error8 = read.csv("out5.csv") #Error of Extrapolation: LR
+
+par(mfrow = c(3, 1))
+plot(error5$STEP, error5$CRR, type = 'l', main = "Error of CRR method",
+     xlab = "STEPS", ylab = "ERROR")
+plot(error5$STEP, error5$BD, type = 'l', main = "Error of BD method",
+     xlab = "STEPS", ylab = "ERROR")
+plot(error6$STEP, error6$LR, type = 'l', main = "Error of LR method",
+     xlab = "STEPS", ylab = "ERROR")
+
+par(mfrow = c(1, 1))
+plot(bound$STEP, bound$Boundary, type = 'l', main = "Exercise boundary",
+     xlab = "STEPS", ylab = "Boundary")
+
+par(mfrow = c(3, 1))
+plot(error7$N, error7$CRR, type = 'l', main = "Error of Extrapolation: CRR",
+     xlab = "N", ylab = "ERROR")
+plot(error7$N, error7$BD, type = 'l', main = "Error of Extrapolation: BD",
+     xlab = "N", ylab = "ERROR")
+plot(error8$N, error8$LR2, type = 'l', main = "Error of Extrapolation: LR",
+     xlab = "N", ylab = "ERROR", ylim = c(-0.001, 0.003), col = 'red')
+lines(error8$N, error8$LR1, col="green")
+abline(h = 0)
