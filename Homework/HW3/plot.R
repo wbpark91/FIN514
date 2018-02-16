651ec0d6bccd66c7420c5791508fb01ab4b759f7
Park <wanbaep2@ug-mac36.ics.illinois.edu>
2018-02-16 17:49:40 -0600

    problem 3 plot

--- Homework/HW3/plot.R
+++ Homework/HW3/plot.R
@@ -52,3 +52,19 @@ plot(error8$N, error8$LR2, type = 'l', main = "Error of Extrapolation: LR",
      xlab = "N", ylab = "ERROR", ylim = c(-0.001, 0.003), col = 'red')
 lines(error8$N, error8$LR1, col="green")
 abline(h = 0)
+
+#------------------------------------------------------------------------------
+## PROBLEM 2
+setwd("/Users/wanbaep2/Desktop/FIN514/Homework/HW3/Q3")
+error9 = read.csv("out.csv") #Error of CRR: continuous barrier option
+
+par(mfrow = c(1, 1))
+plot(error9$STEPS, error9$ERROR, type = 'l', main = "Error of CRR method",
+     xlab = "STEPS", ylab = "ERROR", col = 'red')
+par(new = T)
+with(error9, plot(error9$STEPS, error9$LAMBDA, pch=16, axes=F, xlab=NA, ylab=NA, cex=0.3), col = 'red')
+axis(side = 4)
+mtext(side = 4, line = 0.5, expression(Lambda))
+legend("topright",
+       legend=c("Error", expression(Lambda)),
+       lty=c(1,0), pch=c(NA, 16), col=c("red", "black"))
