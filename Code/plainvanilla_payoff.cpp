473365cfdadb38400dcc66f2963015d912888c97
wbpark91 <wbpark91@gmail.com>
2018-03-19 13:58:31 -0500

    move folder

--- Code/plainvanilla_payoff.cpp
+++
@@ -1,9 +0,0 @@
-#include "plainvanilla_payoff.h"
-#include "u_math.h"
-
-PlainVanillaPayoff::PlainVanillaPayoff(double strike, OptionType type)
-    :strike_(strike), type_(type) {}
-
-double PlainVanillaPayoff::operator()(double s) {
-    return MAX(type_ * (s - strike_), 0.0);
-}
