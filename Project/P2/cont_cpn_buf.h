75007711055bda315fcdb4e5a2f80f00b067c931
wbpark91 <wbpark91@gmail.com>
2018-04-12 20:48:14 -0500

    coupon frequency, correlation add

--- Project/P2/cont_cpn_buf.h
+++ Project/P2/cont_cpn_buf.h
@@ -4,6 +4,8 @@
 #include "payoff.h"
 #include <vector>
 
+enum CpnFreq { Month = 12, Quarter = 4, SemiAnnual = 2, Annual = 1 };
+
 class ContCpnBuf: public Derivative {
 public:
     ContCpnBuf();
@@ -14,24 +16,34 @@ private:
     double mBuffer;
 
     std::vector<double> mCpnSchedule;
+    CpnFreq mFreq;
     bool mTrigger;
+
+    /* correlation */
+    double mCorr;
 };
 
 class ContCpnBufPayoff: public Payoff {
 public:
     /* Constructors and Destructor */
     ContCpnBufPayoff();
-    ContCpnBufPayoff(double spot, double barrier, double coupon,
-                        double buffer);
+    ContCpnBufPayoff(double principal, double barrier,
+        double coupon, double buffer, CpnFreq freq);
     ~ContCpnBufPayoff();
 
     virtual double operator()(double s);
+
+    /* set functions */
     void setTrigger(bool trigger);
+    void setSpot(double spot);
 private:
     double mSpot;
+    double mPrincipal;
     double mBarrier;
     double mCoupon;
     double mBuffer;
+
+    CpnFreq mFreq;
 
     bool mTrigger;
 };
