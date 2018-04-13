c046d7695deddd6c8323509fcf4760ea275859aa
wbpark91 <wbpark91@gmail.com>
2018-04-12 21:06:13 -0500

    ContCpnBuf class

--- Project/P2/cont_cpn_buf.h
+++ Project/P2/cont_cpn_buf.h
@@ -2,13 +2,26 @@
 #define _CONT_CPN_BUF_H_
 #include "derivatives.h"
 #include "payoff.h"
+#include "forex.h"
 #include <vector>
 
 enum CpnFreq { Month = 12, Quarter = 4, SemiAnnual = 2, Annual = 1 };
 
 class ContCpnBuf: public Derivative {
 public:
+    /* Constructors and Destructor */
     ContCpnBuf();
+    ContCpnBuf(double principal, double maturity, double barrier,
+            double buffer, CpnFreq freq);
+    ~ContCpnBuf();
+
+    /* set functions */
+    void setTrigger(bool trigger);
+    void setForex(Forex forex);
+
+    /* pricing functions */
+    virtual double bntprice(unsigned int steps, BinomialType bntType);
+    double fdmprice(unsigned int imax, unsigned int jmax, FDMType fdmType);
 private:
     double mPrincipal;
     double mCoupon;
@@ -21,6 +34,9 @@ private:
 
     /* correlation */
     double mCorr;
+
+    /* Foreign exchange rate*/
+    Forex mForex;
 };
 
 class ContCpnBufPayoff: public Payoff {
